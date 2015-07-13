279959ffcf6f49ff64d486f43cc07f37a1924f7c
Jonatan Tomillo <jonatan.tomillo.1993@gmail.com>
2015-07-13 12:51:45 +0200

    Añadida Base de Datos

---
+++ models/models.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite:
+var sequelize = new Sequelize(null,null,null,
+                    {dialect: "sqlite", storage: "quiz.sqlite"}
+                );
+
+// Importar la definicion de la traba Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+exports.Quiz = Quiz; // exportar definicion de la tabla quiz
+
+sequelize.sync().success(function() {
+  Quiz.count().success(function(count){
+    if(count === 0){
+      Quiz.create({pregunta: 'Capital de Italia',
+                   respuesta: 'Roma'
+                })
+     .success(function(){console.log('Base de datos inicializada')});
+   };
+ });
+});
