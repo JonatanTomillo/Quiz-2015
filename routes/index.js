79dbb9f6e360df4794198a556338f407166d7f7f
Jonatan Tomillo <jonatan.tomillo.1993@gmail.com>
2015-07-15 11:25:24 +0200

    Moderar comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load); //autoLoad :commentId
 
 /* GET author */
 router.get("/author",function(req,res){
@@ -33,5 +34,6 @@ router.delete('/quizes/:quizId(\\d+)',        sessionController.loginRequired, q
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
